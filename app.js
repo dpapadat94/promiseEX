8a9cfb393d84b6d3feb014872cb78507f149293e
dpapadat94 <102779032+dpapadat94@users.noreply.github.com>
2023-03-10 15:13:10 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+/**
+ *
+ * @returns A promise that is designed to resolve with a list of hobbits, or potentially fail with an failure object. The failure object includes a boolean success property and a string message property.
+ */
+
+function getList() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let potentialFail = Math.round(Math.random() * 100) < 10;
+      if (potentialFail) {
+        reject({ success: false, message: "Failed to get list of hobbits." });
+      } else {
+        resolve(["Bilbo", "Frodo", "Sam", "Merry", "Pippin"]);
+      }
+    }, 10);
+  });
+}
+//selecting elemetns
+const error = document.getElementById("error");
+const list = document.getElementById("list");
+
+// TODO: Handle the resolved or rejected states of the promise
+getList()
+  .then((res) => {
+    res.forEach((hobbit) => {
+      let li = document.createElement("li");
+      li.textContent = hobbit;
+      list.appendChild(li);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+    error.textContent = err.message;
+  });
+
+// TODO: If the promise resolves with the list of hobbits
+// Render the list of hobbits as list items within the unordered list with id="list" (check the index.html file)
+
+// TODO: If the promise rejects with the failure object
+// Display the failure message in the paragraph element with id="error" (check index.html file)
